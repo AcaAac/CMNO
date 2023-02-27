21565efa086d80dfd42eadf6b7990c7da1834c35
AcaAac <afonso.d.araujo@tecnico.ulisboa.pt>
2023-02-27 22:01:46 +0000

    sess2

--- lab1.m
+++ lab1.m
@@ -20,16 +20,16 @@ fprintf('C rank: %d\n', rank(Contr));
 disp(' ');
 disp('--------3--------');
 disp('1) Case: x3 is the output.');
-C = [0 0 1 0 0];
-O = obsv(A,C);
+C_temp = [0 0 1 0 0];
+O = obsv(A,C_temp);
 disp('Observabillity matrix:');
 disp(O);
 fprintf('O rank: %d\n', rank(O));
 
 disp(' ');
 disp('2) Case: x1 and x3 are the output.');
-C = [1 0 1 0 0];
-O = obsv(A,C);
+C_temp = [1 0 1 0 0];
+O = obsv(A,C_temp);
 disp('Observabillity matrix:');
 disp(O);
 fprintf('O rank: %d\n', rank(O));
@@ -39,7 +39,7 @@ disp('-------------4-------------');
 disp(size(D));
 D = zeros(1);
 % D = [0, 0, 0, 0, 0]
-[NUM, DEN] = ss2tf(A,B,C,D);
+[NUM, DEN] = ss2tf(A,B,C_temp,D);
 sys = tf(NUM, DEN);
 disp('Transfer function is:');
 sys
@@ -48,7 +48,7 @@ bode(sys)
 
 disp('');
 disp('-------------5------------');
-Q2 = C.' * C
+Q2 = C_temp.' * C_temp
 R = 1
 [K1,S,CLP] = lqr(A, B, Q2, R)
 
@@ -62,28 +62,4 @@ disp(eig_t)
 
 disp(['Real part:' num2str(real(eig_t)')]);
 disp(['Im part:' num2str(imag(eig_t)')]);
-
-disp('');
-disp('-------------6------------');
-
-% Furuta pendulum - State feedback test
-%______________________________________________________________________
-Qr = diag([10,0,1,0,0]); %Weight Matrix for x
-Rr = 1; %Weight for the input variable
-K = lqr(A, B, Qr, Rr); %Calculate feedback gain
-%----------------------------------------------------------------------
-% Simulate controller
-x = [0.1 0 0 0 0]';
-D = [0 0 0 0 0]';
-T = 2; % Time duration of the simulation
-% u = -K * x0;
-sim('statefdbk', T);
-gg = plot(t, x);
-set(gg, 'LineWidth', 1.5)
-gg = xlabel('Time (s9');
-set(gg, 'Fontsize', 14);
-gg = ylabel('\beta (rad)');
-set(gg, 'Fontsize', 14);
-%----------------------------------------------------------------------
-% End of file
 
